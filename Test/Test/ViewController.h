84a14ae6e75b5d2552190b8c6f30e3beb09bf913
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2016-04-15 11:29:21 +0800

    初始化工程

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by yang yang on 16/4/15.
+//  Copyright © 2016年 yang yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by yang yang on 16/4/15.
+//  Copyright © 2016年 yang yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
